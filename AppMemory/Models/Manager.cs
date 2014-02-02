fec9876191b1c126857fdc7705329c80428bc6be
igoryen <igor.yentaltsev@gmail.com>
2014-02-02 17:18:05 -0500

    4) Models/Manager.cs: changes

--- AppMemory/Models/Manager.cs
+++ AppMemory/Models/Manager.cs
@@ -4,50 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace myAppMemory.Models
+namespace CodeFirstOne.Models
 {
     public class Manager
     {
-        public Manager()
-        {
-            this.Students = (List<Student>)HttpContext.Current.Application["Students"];
-        }
-
+        DataContext dc = new DataContext();
         public Student getStudent(int? id)
         {
-            return Students.FirstOrDefault(n => n.Id == id);
+            return dc.Students.FirstOrDefault(n => n.Id == id);
         }
 
         public IEnumerable<Student> sortStudents()
         {
-            return this.Students.OrderBy(n => n.Id);
+            return dc.Students.OrderBy(n => n.Id);
         }
 
         public Student createStudent(Student stu)
         {
-            //stu.Id = Students.Last().Id + 1;
-            stu.Id = Students.Max(n => n.Id) + 1;
-            Students.Add(stu);
+            dc.Students.Add(stu);
+            dc.SaveChanges();
+
             return stu;
         }
 
         public Student createStudent(string fName, string lName, string pNumber, string sid)
         {
             var st = new Student(fName, lName, pNumber, sid);
-            st.Id = Students.Last().Id + 1;
 
-            Students.Add(st);
+            dc.Students.Add(st);
+            dc.SaveChanges();
 
             return st;
         }
         public Student editStudent(int id, string fName, string lName, string pNumber, string sid)
         {
-            var stu = Students.FirstOrDefault(b => b.Id == id);
+            var stu = dc.Students.FirstOrDefault(b => b.Id == id);
 
             stu.FirstName = fName;
             stu.LastName = lName;
             stu.Phone = pNumber;
             stu.StudentNumber = sid;
+
+            dc.SaveChanges();
 
             return stu;
         }
@@ -56,11 +54,8 @@ namespace myAppMemory.Models
         //don't do this until week two.
         public SelectList getSelectList()
         {
-            SelectList sl = new SelectList(Students, "Id", "StudentNumber");
+            SelectList sl = new SelectList(dc.Courses, "Id", "CourseCode");
             return sl;
         }
-
-        
-        public List<Student> Students { get; set; }
     }
 }
