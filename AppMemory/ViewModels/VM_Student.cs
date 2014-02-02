bb9d48e90d0b97f79bd58b0b71fbf505b5200d11
igoryen <igor.yentaltsev@gmail.com>
2014-02-02 17:20:28 -0500

    4) ViewModels/VM_Student.cs: changes

--- AppMemory/ViewModels/VM_Student.cs
+++ AppMemory/ViewModels/VM_Student.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using CodeFirstOne.Models;
 
-namespace myAppMemory.ViewModels
+namespace CodeFirstOne.ViewModels
 {
     public class StudentsForList
     {
@@ -18,6 +19,10 @@ namespace myAppMemory.ViewModels
 
     public class StudentFull : StudentsForList
     {
+        public StudentFull()
+        {
+            this.Courses = new List<CourseForList>();
+        }
         [Required]
         [StringLength(40, MinimumLength = 3)]
         [Display(Name = "First Name")]
@@ -30,10 +35,13 @@ namespace myAppMemory.ViewModels
         [Required]
         [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
         public string Phone { get; set; }
+        public List<CourseForList> Courses { get; set; }
     }
 
     public class StudentPublic
     {
+        [Key]
+        public int StudentId { get; set; }
 
         [Required]
         [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
@@ -51,5 +59,21 @@ namespace myAppMemory.ViewModels
         [Required]
         [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
         public string Phone { get; set; }
+    }
+
+    public class StudentName
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
     }
 }
